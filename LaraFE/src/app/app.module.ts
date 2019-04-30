5b952f2bec57035906ed2454f203240009cf1792
Allianzcortex <iamwanghz@gmail.com>
2019-04-30 13:45:18 -0300

    add front end post request function

--- LaraFE/src/app/app.module.ts
+++ LaraFE/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SimpleHTTPComponent } from './SimpleHTTPComponent';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleHTTPComponent
   ],
   imports: [
     BrowserModule,
