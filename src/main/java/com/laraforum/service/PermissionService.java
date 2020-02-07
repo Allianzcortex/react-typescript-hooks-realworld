93758d76af93499d8d6a8156ea6a1c448d05d8d3
iamwanghz@gmail.com <iamwanghz@gmail.com>
2020-02-07 16:40:20 -0400

    add docker support

--- src/main/java/com/laraforum/service/PermissionService.java
+++
@@ -1,9 +0,0 @@
-package com.laraforum.service;
-
-import com.laraforum.model.enums.Permission;
-
-import java.util.Optional;
-
-public interface PermissionService {
-    Optional<Permission> findByPermissionNumber(Integer permissionNumber);
-}
