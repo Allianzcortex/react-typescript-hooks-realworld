1b98c72820eea29e290f01ffef0a3b1e07c2bc96
Allianzcortex <iamwanghz@gmail.com>
2021-06-11 12:13:51 -0300

    fix pagination bug - handle float number

--- src/components/Home/Pagination.tsx
+++ src/components/Home/Pagination.tsx
@@ -1,7 +1,7 @@
 import React, { Dispatch, SetStateAction, SyntheticEvent } from "react";
 import { Icon, Pagination as SemanticPagination } from "semantic-ui-react";
 import { PER_PAGE_COUNT } from "../../utils";
-import './style.css'
+import "./style.css";
 
 interface IProps {
   count: number; // all count
@@ -15,12 +15,10 @@ interface IData {
 }
 
 export const Pagination = ({ count, currentPage, setCurrentPage }: IProps) => {
-  console.log(count)
   const totalPages =
     count % PER_PAGE_COUNT == 0
-      ? count / PER_PAGE_COUNT
-      : count / PER_PAGE_COUNT + 1;
-    console.log(totalPages)
+      ? Math.floor(count / PER_PAGE_COUNT)
+      : Math.floor(count / PER_PAGE_COUNT) + 1;
 
   const handlePageChange = (event: SyntheticEvent, data: object) => {
     // TODO check whether we can handle it better (no need to use Number to force conversion)
